005fee0eeae2ffc9c9f9bb6493ec707da253c161
Alicia Piedra <aliciapiedra98@gmail.com>
2019-01-08 09:58:13 -0500

    about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,7 @@ class About extends React.Component {
 	render() {
 		return(
 			<div className="aboutCard">
-				<h2>{this.props.title}</h2>
+				<h2>About Me</h2>
 				<div className="line" style={{backgroundColor: this.props.bgc}}></div>
 				<p>{this.props.content}</p>
 			</div>
