750a7c821cf6073eea7e35e2f2333adf9d249810
Alicia Piedra <aliciapiedra98@gmail.com>
2019-01-11 01:01:59 -0500

    updated my cards for resume, projects, contact

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,22 @@
 import React, { Component } from 'react'; 
-import '../css/cards.css';
+import '../css/projects.css';
 // import @bootstrap
 
-class Carousel extends Component {
+class Projects extends Component {
     render() {
         return (
-            <Carousel>
-            <Carousel.Item>
-              <img width={900} height={500} alt="900x500" src="/carousel.png" />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img width={900} height={500} alt="900x500" src="/carousel.png" />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img width={900} height={500} alt="900x500" src="/carousel.png" />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+          <div className="projectsCard">
+          <div class="projects-card spring-fever">
+            <div class="title-content">
+              <h3>Projects</h3>
+          </div>
+            <div class="projects-intro">Full Stack Software Developer</div>
+          <div class="gradient-overlay"></div>
+          <div class="color-overlay"></div>
+          </div>
+          </div>		
         )
     }
 }
 
-export default Carousel
+export default Projects
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,25 +1,19 @@
 import React, { Component }from 'react';
-import '../css/cards.css'; 
+import '../css/resume.css'; 
 
 class Resume extends Component {
     render() {
         return (
-            <div className="slideInLeft">
-            <div className="aboutCard">
-			<div class="blog-card spring-fever">
+            <div className="resumeCard">
+			<div class="resume-card spring-fever">
   			<div class="title-content">
     			<h3>Resume</h3>
-    		<div class="intro">Full Stack Software Developer</div>
-  			</div>
-  			<div class="card-info">
-    			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim... 
-				<a href="#">Read Article<span class="licon icon-arr icon-black"></span></a>
 			</div>
+    		<div class="resume-intro">Full Stack Software Developer</div>
 			<div class="gradient-overlay"></div>
 			<div class="color-overlay"></div>
             </div>
-			</div>
-			</div>			
+			</div>		
         )
     }
 }
