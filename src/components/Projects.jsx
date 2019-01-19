85dadfd671102aeebd92ad8355ced1edd4110ae3
Alicia Piedra <aliciapiedra98@gmail.com>
2019-01-19 14:34:05 -0500

    guardian is up there

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'; 
 import '../css/projects.css';
+import GuardianLogo from '../images/widget-logo.png'
 // import @bootstrap
 
 class Projects extends Component {
@@ -11,9 +12,9 @@ class Projects extends Component {
               <h3>Projects</h3>
           </div>
             <div class="projects-intro">Full Stack Software Developer</div>
-              <div className="Guardian">
-                <img src="../images/widget-logo-guardian.png" alt="Guardian" />
-                
+              <div className="guardianLogo">
+              <a href="https://guardian-app-wyncode.herokuapp.com/"><img src={GuardianLogo} ></img></a>
+                <p>Guardian: React on Rails Application. A digital bodyguard whenever you're feeling unsafe.</p>
               </div>
           <div class="gradient-overlay"></div>
           <div class="color-overlay"></div>
