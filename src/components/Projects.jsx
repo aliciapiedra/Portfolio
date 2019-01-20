e581da64b69880272037eea48d1204e183e38390
Alicia Piedra <aliciapiedra98@gmail.com>
2019-01-19 20:23:43 -0500

    few more touches

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,11 +14,11 @@ class Projects extends Component {
           </div>
             <div class="projects-intro">Full Stack Software Developer</div>
               <div className="guardianLogo">
-              <a href="https://guardian-app-wyncode.herokuapp.com/"><img src={GuardianLogo} ></img></a>
+              <a href="https://guardian-app-wyncode.herokuapp.com/"><img src={GuardianLogo} alt="Guardian"></img></a>
                 <p>Guardian: React on Rails Application. A digital bodyguard whenever you're feeling unsafe.</p>
               </div>
               <div className="localLogo">
-                <a href="https://localbrew.herokuapp.com/"><img className="logoForLocal" src={BrewLogo}></img></a>
+                <a href="https://localbrew.herokuapp.com/"><img className="logoForLocal" src={BrewLogo} alt="Local Brew"></img></a>
                 <p>Local Brew: React Application. Find local breweries near you!</p>
               </div>
           <div class="gradient-overlay"></div>
